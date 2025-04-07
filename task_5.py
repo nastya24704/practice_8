8db358f3c9940a9f6d569a44f8955223f7673fd5
nastya24704 <nastazeravina@gmail.com>
2025-04-07 07:43:12 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,9 +1,9 @@
-number = input()
+number = int(input())
 summ = 0
-for i in range(1, len(number)+1):
-    summ += int(number)%10
-    number = int(number)//10
-if number % summ ==0:
+for i in range(1, number):
+    if number % i == 0:
+        summ +=i
+if number % summ == 0:
     print('совершенное')
 else:
     print('несовершенное')
