133474c891952f3640b9220ddd2d532556fa23ec
nastya24704 <nastazeravina@gmail.com>
2025-04-07 09:11:44 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,20 +1,11 @@
 number = int(input())
 summ = 0
-for i in range(1, number):
-    if number % i == 0:
-        summ +=i
-if number == summ:
-    print('совершенное')
-else:
-    print('несовершенное')
-
-
-number = int(input())
-summ = 0
+a = 0
 for r in range(1, number):
     for i in range(1, r):
         if r % i == 0:
             summ += i
     if r == summ:
-        print(r)
+        a+=1
     summ = 0
+print(a)
