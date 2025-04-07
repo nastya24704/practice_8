ce042ea1595a632a482f5e54d4ae0e3117797195
nastya24704 <nastazeravina@gmail.com>
2025-04-07 07:54:04 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -3,7 +3,7 @@ summ = 0
 for i in range(1, number):
     if number % i == 0:
         summ +=i
-if number % summ == 0:
+if number == summ:
     print('совершенное')
 else:
     print('несовершенное')
